475025097e77c101c84c6106b16c96d4c6d33f50
Eden <edensinclair15@gmail.com>
2020-12-16 19:40:15 +0000

    Please work now

--- src/components/gallery/gallery.css.js
+++ src/components/gallery/gallery.css.js
@@ -4,9 +4,9 @@ import MEDIA from 'helpers/mediaTemplates';
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 2fr);
-  grid-gap: 3rem;
+  grid-gap: 3vh;
   padding: 0 0rem;
-  margin: 2rem 0 0 0;
+  margin: 0rem 0 0 0;
 
   
   ${MEDIA.TABLET`
